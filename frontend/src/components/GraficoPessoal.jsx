8c4bc302d1f715eea57d2caaf0e51b75299e7677
taykas <thaynaschaeffer@gmail.com>
2026-07-08 09:42:46 -0300

    feat: starting building graphics

--- frontend/src/components/GraficoPessoal.jsx
+++ frontend/src/components/GraficoPessoal.jsx
@@ -1,5 +1,131 @@
-import { Bar } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from "chart.js";
+
+import { Line } from "react-chartjs-2";
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 
 export const GraficoPessoal = () => {
-    return <Bar data={{}} />;
+
+
+    const data = {
+
+        labels: [
+            "2025 T2",
+            "2025 T3",
+            "2025 T4",
+            "2025 T1",
+            "2026 T2"
+        ],
+
+        datasets: [
+            {
+                label: "Avaliação",
+
+                data: [
+                    70,
+                    76,
+                    82,
+                    85,
+                    90
+                ],
+
+                borderWidth: 3,
+
+                tension: 0.4, // deixa a linha mais suave
+
+                pointRadius: 5,
+
+                pointHoverRadius: 7
+            }
+        ]
+    };
+
+
+    const options = {
+
+        responsive: true,
+
+        maintainAspectRatio: false,
+
+        plugins: {
+
+            legend: {
+                display: false
+            },
+
+            tooltip: {
+                enabled: true
+            }
+
+        },
+
+
+        scales: {
+
+            y: {
+
+                min: 60,
+                max: 100,
+
+                ticks: {
+                    callback: (value) => `${value}%`
+                },
+
+                grid: {
+                    display: false
+                }
+
+            },
+
+
+            x: {
+
+                grid: {
+                    display: false
+                }
+
+            }
+
+        }
+
+    };
+
+
+    return (
+        <div className="
+            w-[25vw]
+            h-[25vh]
+            rounded-2xl
+            bg-white
+            shadow-2xl
+            ring-1
+            ring-gray-200
+            p-4
+        ">
+            <Line
+                data={data}
+                options={options}
+            />
+        </div>
+    );
+
 };
--- frontend/src/pages/UserMain.jsx
+++ frontend/src/pages/UserMain.jsx
@@ -2,6 +2,8 @@ import { Header } from "../components/Header";
 import { CardNotificacaoAvaliacao } from "../components/CardNotificacaoAvaliacao";
 import { GraficoPessoal } from "../components/GraficoPessoal";
 
+import bars from "../assets/bars.png";
+
 export const UserMain = () => {
     return (
         <main className="user-main">
@@ -18,8 +20,42 @@ export const UserMain = () => {
 
                 <h1 className="font-bold text-3xl mb-8 mt-15">Meu Desempenho</h1>
 
-                <div className="w-[800px] h-[400px] mx-auto">
-                    <GraficoPessoal />
+                
+                <div className="flex flex-row justify-around">
+
+                    <section className="w-[40%]">
+
+                        <div className="flex flex-row items-center gap-5">
+                            <div className="p-2 bg-[#0291F7]/15 w-17 items-center flex justify-center rounded-xl">
+                                <img src={bars} alt="" className="w-10"/>
+                            </div>
+
+                            <h1 className="font-bold text-xl">Desempenho Comparativo</h1>
+                        </div>
+
+                        <div className="flex flex-row">
+                            <section>
+                                <h1>Resultado Atual</h1>
+                                <h1>90%</h1>
+                            </section>
+
+                            <GraficoPessoal />
+                            
+                        </div>
+
+
+                    </section>
+
+                    <section className="w-[40%]">
+
+                        <div className="p-2 bg-[#0291F7]">
+                            <img src={bars} alt="" className="w-10"/>
+                        </div>
+
+                        <GraficoPessoal />
+
+                    </section>
+
                 </div>
             </div>
         </main>
