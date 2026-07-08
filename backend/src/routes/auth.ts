a91f09571bcdc4ce010a5591246d227b182acb3e
Heloise18 <heloisezelma18@gmail.com>
2026-07-08 09:04:40 -0300

    feat: routes created

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,10 @@
+
+import express from "express";
+
+const route = express.Router()
+
+route 
+    .post('/register')
+    .post('/login')
+
+export default route;
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const route = express.Router();
+
+route
+    .post('/createUser')
+    .get('/findById/:id')
+    .get('/findAll')
+    .put('/update/:id')
+    .delete('/delete/:id')
+
+export default route;
