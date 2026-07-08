2fa0799bad3b8f0def29ba252097ae8384826aae
KetyJomes <jomessofiaketlyn@gmail.com>
2026-07-08 08:36:11 -0300

    style(login): refine login page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import './App.css'
 
+import {Login} from "./pages/Login.jsx"
 import {UserMain} from "./pages/UserMain.jsx"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/login-page" element={<Login />} />
           <Route path="/user-main" element={<UserMain />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import background from "../assets/supergraphic.svg";
 
-export default function Login() {
+export const Login = () => {
   return (
     <div
       className="min-h-screen flex items-center justify-center bg-center bg-cover"
@@ -9,18 +9,14 @@ export default function Login() {
       }}
     >
       <div className="bg-white w-[90%] max-w-6xl h-[650px] rounded-lg shadow-lg flex">
-
-        {/* Lado esquerdo */}
         <div className="w-1/2 flex items-center justify-center">
           <h1 className="text-6xl font-bold text-black">
             SocialHub
           </h1>
         </div>
 
-        {/* Linha divisória */}
         <div className="w-px bg-gray-300 my-16"></div>
 
-        {/* Lado direito */}
         <div className="w-1/2 flex flex-col justify-center px-16">
 
           <h2 className="text-5xl font-bold mb-12">
@@ -29,30 +25,27 @@ export default function Login() {
 
           <div className="mb-8">
             <label className="block mb-2 text-lg">EDV:</label>
-
             <input
               type="text"
               placeholder="Digite seu EDV"
-              className="w-full border-b border-gray-400 outline-none py-2"
+              className="w-full border-b border-gray-400 outline-none py-2 placeholder:text-gray-300"
             />
           </div>
 
           <div className="mb-8">
             <label className="block mb-2 text-lg">Senha:</label>
-
-            <input
-              type="password"
-              placeholder="Digite sua senha"
-              className="w-full border-b border-gray-400 outline-none py-2"
-            />
+              <input
+                type="password"
+                placeholder="Digite sua senha"
+                className="w-full border-b border-gray-400 outline-none py-2 placeholder:text-gray-300"
+              />
           </div>
 
-          <a
-            href="#"
-            className="text-right underline mb-10"
-          >
-            Se Cadastrar
-          </a>
+          <div className="flex justify-center mb-10">
+            <a href="#" className="underline">
+              Se Cadastrar
+            </a>
+          </div>
 
           <button className="bg-gray-300 hover:bg-gray-400 text-white font-bold py-3 rounded-xl transition">
             Entrar
