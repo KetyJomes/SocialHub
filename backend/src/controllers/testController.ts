a7a7824edf88f250db9c68b2d24a689ed78ceeb9
gatosembucho <malu.14geraldo@gmail.com>
2026-07-09 11:18:29 -0300

    teste

---
+++ backend/src/controllers/testController.ts
@@ -0,0 +1,30 @@
+import {Request, Response} from 'express';
+import { createTestDTO } from '../dtos/testDTO.ts';
+import { createTest, showTest, showTests } from '../services/testServices.ts';
+import test from 'node:test';
+
+export default class TestController {
+    static async create(req:Request, res: Response){
+        const data: createTestDTO = req.body;
+        try{
+            await createTest(data);
+
+            return res.status(201).send({ response: "Prova finalizada"});
+        }
+        catch(e) {
+            return res.status(500).send({response: "Ocorreu algum erro no servidor"})
+        }
+    }
+
+    static async show(req: Request, res: Response){
+        const id = parseInt(req.params[0],10);
+        try{
+            const user = await showTest(id);
+            return res.status(200).send(test)
+
+        }
+        catch(e){
+            return res.status(500).send({ response: 'Ocorreu algum erro no servidor'})
+        }
+    }
+}
