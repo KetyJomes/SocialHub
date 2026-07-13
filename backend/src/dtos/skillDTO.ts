17fbd1d3b00e363705090b81fc2fe9a73698725b
Heloise18 <heloisezelma18>
2026-07-13 10:06:11 -0300

    Feat: service skill

---
+++ backend/src/dtos/skillDTO.ts
@@ -0,0 +1,5 @@
+
+export interface createSkillDTO{
+    title: string,
+    description: string 
+}
---
+++ backend/src/services/skillService.ts
@@ -0,0 +1,10 @@
+// import { createSkillDTO } from "../dtos/skillDTO.ts"
+// import {prisma} from "../lib/prisma.ts"
+
+// export const createSkill = async(data: createSkillDTO)=>{
+//     const { title, description } = data
+
+//     return await prisma.skill.
+// }
+
+
